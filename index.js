e8d91e26935f55b365b36deb585df69786710256
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2020-12-08 14:33:33 +0100

    add build

---
+++ index.js
@@ -0,0 +1,43 @@
+let mustache = require("mustache");
+let fs = require("fs");
+
+let players = [
+  {
+    pseudo: "Pursuable",
+    rank: 1,
+    score: 200,
+  },
+  {
+    pseudo: "Mecadie",
+    rank: 2,
+    score: 155,
+  },
+  {
+    pseudo: "AikoChikana",
+    rank: 3,
+    score: 135,
+  },
+  {
+    pseudo: "youf-olivier",
+    rank: 4,
+    score: 115,
+  },
+];
+
+let file = fs.readFile("./index.html", "utf8", (err, data) => {
+  let view = players.sort((a, b) => a.rank - b.rank);
+  let index = mustache.render(data, {
+    players: view.slice(3),
+    podium: view.slice(0, 3),
+  });
+
+  let dir = "./build";
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.writeFile("build/index.html", index, "utf8", (err) => {
+    console.log(err);
+  });
+});
