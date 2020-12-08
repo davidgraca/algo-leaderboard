1be63d49bbe185da84c1277a09d301a9f29bbd12
Rémi FRUTEAU DE LACLOS <remi.fruteaudelaclos@axa.fr>
2020-12-08 16:43:52 +0100

    clashes json

--- index.js
+++ index.js
@@ -14,40 +14,40 @@ let callback = (err) => {
   console.log("source.txt was copied to destination.txt");
 };
 
-const idClashs = ["1427616e3b02df57113addab9155f675ab25569", "142762261b493c7d8c02c1dd24f7dfe98c75c73"];
-const challenger = "djedje72";
+const allClashes = JSON.parse(fs.readFileSync("./clashes.json", "utf8"));
+
 const ranks = [100,80,60,40,20];
 const ignore = ["djedje72", "Coldk", "J7N__", "Jean-Lou"];
 const beatChamp = 75;
 const completed = 25;
 
-const computeChamp = (players) => {
-  const champion = players.find(({codingamerNickname}) => codingamerNickname === challenger);
+const computeChamp = (players, champion) => {
+  const championEl = players.find(({codingamerNickname}) => codingamerNickname === champion);
   return players
-      .filter(e => e !== champion && !ignore.includes(e.codingamerNickname))
+      .filter(e => e !== championEl && !ignore.includes(e.codingamerNickname))
       .map(({codingamerNickname, score},i) => ({
           "pseudo": codingamerNickname,
           // "rank": i+1,
-          "score": (ranks[i] || 0) + (players.indexOf(champion) > i ? beatChamp : 0) + (score === 100 ? completed : 0)
+          "score": (ranks[i] || 0) + (players.indexOf(championEl) > i ? beatChamp : 0) + (score === 100 ? completed : 0)
       }))
 }
 
 (async() => {
   const data = fs.readFileSync("./index.html", "utf8");
 
-  const clashes = await Promise.all(idClashs.map(async idClash => {
+  const clashes = await Promise.all(allClashes.map(async({id, champion}) => {
     const {players} = await fetch("https://www.codingame.com/services/ClashOfCode/findClashReportInfoByHandle", {
       agent,
       "headers": {
         "accept": "application/json, text/plain, */*",
         "content-type": "application/json;charset=UTF-8",
       },
-      "body": JSON.stringify([idClash]),
+      "body": JSON.stringify([id]),
       "method": "POST"
     }).then(e=>e.json());
     players.sort((a,b) => a.rank - b.rank);
 
-    return computeChamp(players);
+    return computeChamp(players, champion);
   }));
 
   const globalChamps = Object.entries(clashes
@@ -67,7 +67,7 @@ const computeChamp = (players) => {
 
 
   let index = mustache.render(data, {
-    "idClashs": idClashs.map((id, i) => ({id, "index": i + 1})),
+    "clashes": allClashes.map(({id}, i) => ({id, "index": i + 1})),
     "podium": globalChamps.slice(0, 3),
     "players": globalChamps.slice(3)
   });
