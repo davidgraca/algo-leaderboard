e9f6d86d03c63d076a4b8c4e85ce340186936888
Rémi FRUTEAU DE LACLOS <remi.fruteaudelaclos@axa.fr>
2020-12-09 09:54:58 +0100

    handle multiple sessions

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let callback = (err) => {
   console.log("source.txt was copied to destination.txt");
 };
 
-const allClashes = JSON.parse(fs.readFileSync("./clashes.json", "utf8")).session1;
+const allClashes = JSON.parse(fs.readFileSync("./clashes.json", "utf8"));
 const ranks = [100, 80, 60, 40, 20];
 const ignore = ["djedje72", "Coldk", "J7N__", "Jean-Lou"];
 const beatChamp = 75;
@@ -34,46 +34,51 @@ const computeChamp = (players, champion) => {
 (async () => {
   const data = fs.readFileSync("./index.html", "utf8");
 
-  const clashes = await Promise.all(
-    allClashes.map(async ({ id, champion }) => {
-      const { players } = await fetch("https://www.codingame.com/services/ClashOfCode/findClashReportInfoByHandle", {
-        agent,
-        headers: {
-          accept: "application/json, text/plain, */*",
-          "content-type": "application/json;charset=UTF-8",
-        },
-        body: JSON.stringify([id]),
-        method: "POST",
-      }).then((e) => e.json());
-      players.sort((a, b) => a.rank - b.rank);
+  const classement = await Promise.all(
+    allClashes.map(async session => {
+      const clashes = await Promise.all(
+        session.map(async ({ id, champion }) => {
+          const { players } = await fetch("https://www.codingame.com/services/ClashOfCode/findClashReportInfoByHandle", {
+            agent,
+            headers: {
+              accept: "application/json, text/plain, */*",
+              "content-type": "application/json;charset=UTF-8",
+            },
+            body: JSON.stringify([id]),
+            method: "POST",
+          }).then((e) => e.json());
+          players.sort((a, b) => a.rank - b.rank);
 
-      return computeChamp(players, champion);
+          return computeChamp(players, champion);
+        })
+      );
+
+      const globalChamps = Object.entries(
+        clashes.flat().reduce(
+          (acc, { pseudo, score }) => ({
+            ...acc,
+            [pseudo]: (acc[pseudo] || 0) + score,
+          }),
+          {}
+        )
+      )
+      .sort(([, a], [, b]) => b - a)
+      .map(([pseudo, score], i) => ({
+        pseudo,
+        score,
+        rank: i + 1,
+      }));
+      return  {
+        clashes: session.map(({ id }, i) => ({ id, index: i + 1 })),
+        podium: globalChamps.slice(0, 3),
+        players: globalChamps.slice(3),
+      };
     })
   );
 
-  const globalChamps = Object.entries(
-    clashes.flat().reduce(
-      (acc, { pseudo, score }) => ({
-        ...acc,
-        [pseudo]: (acc[pseudo] || 0) + score,
-      }),
-      {}
-    )
-  )
-    .sort(([, a], [, b]) => b - a)
-    .map(([pseudo, score], i) => ({
-      pseudo,
-      score,
-      rank: i + 1,
-    }));
+  console.log(classement);
 
-  console.log(globalChamps);
-
-  let index = mustache.render(data, {
-    clashes: allClashes.map(({ id }, i) => ({ id, index: i + 1 })),
-    podium: globalChamps.slice(0, 3),
-    players: globalChamps.slice(3),
-  });
+  let index = mustache.render(data, {"classement":classement.map((score, i) => ({"index":i+1, score}))});
 
   let dir = "./build";
 
